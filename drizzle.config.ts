2fd435da9b94240d1788e463aad0e3de9e83e43f
Sedat Uygur <sedat.uygur@outlook.com>
2024-11-24 21:27:56 +0300

    🎉 design database schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,9 +3,9 @@ if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
-
+	out: "./drizzle",
 	dbCredentials: {
-		url: process.env.DATABASE_URL
+		url: process.env.DATABASE_URL!
 	},
 
 	verbose: true,
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,16 +1,51 @@
-import { pgTable, serial, text, integer, timestamp } from 'drizzle-orm/pg-core';
+import { sql } from "drizzle-orm";
+import { pgTable, pgEnum, serial, text, integer, timestamp, uuid } from 'drizzle-orm/pg-core';
 
-export const user = pgTable('user', {
-	id: text('id').primaryKey(),
+export const enumFeedItemType = pgEnum('FeedItemType', ['YouTubeChannel']);
+export const enumVisiblity = pgEnum('Visiblity', ['Public', 'Unlisted', 'Private']);
+
+export const user = pgTable('User', {
+	id: uuid('id').notNull().default(sql`gen_random_uuid()`).primaryKey(),
 	age: integer('age'),
 	username: text('username').notNull().unique(),
-	passwordHash: text('password_hash').notNull()
+	passwordHash: text('password_hash').notNull(),
+	createdAt: timestamp('createdAt').notNull().defaultNow(),
+	updatedAt: timestamp('updatedAt').notNull(),
+});
+
+export const feed = pgTable('Feed', {
+	id: uuid('id').notNull().default(sql`gen_random_uuid()`).primaryKey(),
+	name: text('name').notNull(),
+	description: text('description'),
+	visibility: enumVisiblity('visibility').notNull(),
+	userId: uuid('userId').notNull().references(() => user.id),
+	createdAt: timestamp('createdAt').notNull().defaultNow(),
+	updatedAt: timestamp('updatedAt').notNull(),
+});
+
+export const feedItem = pgTable('FeedItem', {
+	id: serial('id').notNull().primaryKey(),
+	name: text('name').notNull(),
+	description: text('description'),
+	feedId: uuid('feedId').notNull().references(() => feed.id),
+	feedItemMetaId: uuid('feedItemMetaId').notNull().references(() => feedItemMeta.id),
+	createdAt: timestamp('createdAt').notNull().defaultNow(),
+	updatedAt: timestamp('updatedAt').notNull(),
+});
+
+export const feedItemMeta = pgTable('FeedItemMeta', {
+	id: uuid('id').notNull().default(sql`gen_random_uuid()`).primaryKey(),
+	name: text('name').notNull(),
+	originId: text('originId').notNull(),
+	type: enumFeedItemType('type').notNull(),
+	createdAt: timestamp('createdAt').notNull().defaultNow(),
+	updatedAt: timestamp('updatedAt').notNull(),
 });
 
 export const session = pgTable('session', {
 	id: text('id').primaryKey(),
-	userId: text('user_id')
-		.notNull()
+	userId: uuid('user_id')
+		.notNull().default(sql`gen_random_uuid()`)
 		.references(() => user.id),
 	expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull()
 });
